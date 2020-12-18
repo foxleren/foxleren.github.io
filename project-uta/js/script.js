ffcf77992abf8f088320323ff1d8c008217bb833
foxleren <64990498+foxleren@users.noreply.github.com>
2020-12-18 15:30:48 +0300

    add header

---
+++ project-uta/js/script.js
@@ -0,0 +1,112 @@
+// NAVIGATION LOGO SCROLL TOP
+ /*
+$('.logo').on('click', function (e) {
+	e.preventDefault();
+	$('.nav-toggle').removeClass('open');
+	$('.side-menu').removeClass('full');
+	$('html, body').animate({
+		scrollTop: 0
+	}, 750, 'easeInOutQuad')
+}); */
+// LINKS TO ANCHORS 
+/*
+$('a[href^="#"]').on('click', function (event) {
+
+	var $target = $(this.getAttribute('href'));
+
+	if ($target.length) {
+		event.preventDefault();
+		$('html, body').stop().animate({
+			scrollTop: $target.offset().top
+		}, 750, 'easeInOutQuad');
+	}
+});  */
+
+// TOGGLE HAMBURGER & COLLAPSE NAV
+$('.nav-toggle').on('click', function () {
+	$(this).toggleClass('open');
+	$('.side-menu').toggleClass('full');
+});
+// REMOVE X & COLLAPSE NAV ON ON CLICK
+$('.side-menu a').on('click', function () {
+	$('.nav-toggle').removeClass('open');
+	$('.side-menu').removeClass('full');
+});
+
+// SHOW/HIDE NAV
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function (event) {
+	didScroll = true;
+});
+
+setInterval(function () {
+	if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250);
+
+function hasScrolled() {
+	if($('.side-menu').is('.full')) {
+		var st = $(this).scrollTop();
+
+	// Make sure they scroll more than delta
+	if (Math.abs(lastScrollTop - st) <= delta)
+		return;
+
+	// If they scrolled down and are past the navbar, add class .nav-up.
+	// This is necessary so you never see what is "behind" the navbar.
+	if (st > lastScrollTop && st > navbarHeight) {
+		// Scroll Down
+		$('header').removeClass('show-nav').addClass('hide-nav');
+		$('.nav-toggle').removeClass('open');
+		$('.side-menu').removeClass('full');
+	} else {
+		// Scroll Up
+		if (st + $(window).height() < $(document).height()) {
+			$('header').removeClass('hide-nav').addClass('show-nav');
+		}
+	}
+
+	lastScrollTop = st;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------- */
+
+
+let fields = document.querySelectorAll('.upload-file-input');
+    Array.prototype.forEach.call(fields, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.upload-file-status').innerText;
+ 
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+ 
+        if (countFiles)
+          label.querySelector('.upload-file-status').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.upload-file-status').innerText = labelVal;
+      });
+    });
+
+
