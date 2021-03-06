5755afce7cd0a325aec8a7d15c12c5933ce01f7e
foxleren <64990498+foxleren@users.noreply.github.com>
2021-03-07 00:05:50 +0300

    add Achievments page

--- Chess.Club/js/script.js
+++ Chess.Club/js/script.js
@@ -1,6 +1,22 @@
+/*-- Появление меню --*/
 let menu = document.querySelector('.menu-link-container')
 let menuWindow = document.querySelector('.menu-window')
 
 menu.addEventListener('click', function () {
     menuWindow.classList.toggle('open')
 })
+/*-- Анимация появления и исчезновения награды --*/
+let trophy = document.querySelectorAll('.trophy-container')
+let trophyBig = document.querySelectorAll('.trophyBig-background')
+
+for (let i = 0; i < trophy.length; i++) {
+    trophy[i].addEventListener("click", function() {
+        trophyBig[i].classList.toggle('open')
+    })
+}
+
+for (let i = 0; i < trophyBig.length; i++) {
+    trophyBig[i].addEventListener("click", function() {
+        trophyBig[i].classList.toggle('open')
+    })
+}
